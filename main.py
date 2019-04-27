664ddf62915813c8f38c80a2724a6fcd7960b701
Casey Stratton <cstratto@email.uark.edu>
2019-04-27 18:24:07 -0500

    trying shit.

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 from keras.models import Sequential
-from keras.layers import Dense, Activation
-from keras.optimizers import Adam
+from keras.layers import Dense, Dropout
+from keras.optimizers import Adam, SGD
+from keras.callbacks import EarlyStopping
 
 from data import get_data
+
+from sklearn.utils import compute_class_weight
 
 x_train, x_test, y_train, y_test = get_data()
 
 model = Sequential([
-    Dense(output_dim=100, input_dim=len(x_train.columns)),  # input layer
-    Activation('relu'),  # normalize values to within range -1 to 1
-    Dense(output_dim=64),  # hidden layer #1
-    Activation('relu'),
-    Dense(output_dim=64),  # hidden layer #2
-    Activation('relu'),
-    Dense(output_dim=10),  # hidden layer #3
-    Activation('relu'),
-    Dense(output_dim=1),  # output layer
-    Activation('softmax')
+    Dense(output_dim=100, activation='relu', input_dim=len(x_train.columns)),  # input layer
+    Dense(output_dim=50, activation='sigmoid'),
+    Dense(output_dim=50, activation='relu'),
+    Dense(output_dim=1, activation='softmax'),  # output layer
 ])
 
-model.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.01), metrics=['accuracy'])
+model.compile(loss='mean_squared_error', optimizer=SGD(lr=1e-6, decay=1e-2), metrics=['accuracy'])
 # loss is our cost function
 
-model.fit(x_train, y_train, epochs=5, batch_size=32)  # backprop arguments
+model.fit(x_train, y_train, epochs=100, batch_size=16, callbacks=[EarlyStopping(monitor='loss', patience=0)])  # backprop arguments
 
-loss_and_metrics = model.evaluate(x_test, y_test, batch_size=32)
+loss_and_metrics = model.evaluate(x_test, y_test, batch_size=16)
 
 print(loss_and_metrics)
